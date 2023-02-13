8d6e163f0d81701c5763caf1129d5163bbfd409c
soohyun <soohyun_kim2@tmax.co.kr>
2023-02-13 18:40:01 +0900

    tabsnav

--- src/navigators/StackNavigator.tsx
+++ src/navigators/StackNavigator.tsx
@@ -1,16 +1,13 @@
 import React from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import TabsNav from './TabsNavigator';
 
 const Stack = createStackNavigator();
 
 const StackNavigator = () => {
   return (
     <Stack.Navigator screenOptions={{ presentation: 'modal' }}>
-      <Stack.Screen
-        name="홈"
-        options={{ headerShown: false }}
-        component={() => <div>test</div>}
-      />
+      <Stack.Screen name="홈" options={{ headerShown: false }} component={TabsNav} />
       <Stack.Screen
         name="캘린더"
         options={{ headerShown: false }}
